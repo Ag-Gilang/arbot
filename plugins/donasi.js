7ffd45e01c44ec2d2ad82667c3fe10237abcf4ec
ARYA GILANG <91578406+Ag-Gilang@users.noreply.github.com>
2021-11-09 19:15:38 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
-┌〔 Donasi • Emoney 〕
-├ https://saweria.co/ariffb
-├ https://trakteer.id/ariffb/tip
+┌〔 Donasi • Pulsa 〕
+├ 3 three  : 089678391870
+├ Telkomsel: 081211453433
 └────
-`.trim(), '© stikerin', 'Donasi', '.donasi', m)
+`.trim(), '© Agbot', 'Donasi', '.donasi', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
