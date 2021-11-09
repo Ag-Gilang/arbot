3f1803cb78d0e80ca0226d053ae81450c5fd725f
ARYA GILANG <91578406+Ag-Gilang@users.noreply.github.com>
2021-11-09 22:33:31 +0700

    Update tekateki.js

--- plugins/tekateki.js
+++ plugins/tekateki.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let timeout = 120000
-let poin = 500
+let poin = 1000
 let handler = async (m, { conn, usedPrefix }) => {
     conn.tekateki = conn.tekateki ? conn.tekateki : {}
     let id = m.chat
@@ -18,10 +18,10 @@ Ketik ${usedPrefix}tete untuk bantuan
 Bonus: ${poin} XP
 `.trim()
     conn.tekateki[id] = [
-        await conn.send2Button(m.chat, caption, '© stikerin', 'Bantuan', `.tete`, m),
+        await conn.send2Button(m.chat, caption, '© Agbot', 'Bantuan', `.tete`, m),
         json, poin,
         setTimeout(async () => {
-            if (conn.tekateki[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, '© stikerin', 'Teka Teki', `.tekateki`, conn.tekateki[id][0])
+            if (conn.tekateki[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, '© Agbot', 'Teka Teki', `.tekateki`, conn.tekateki[id][0])
             delete conn.tekateki[id]
         }, timeout)
     ]
