1063eadf6067a8717db02fd3d5f7999a6d6352a1
ARYA GILANG <91578406+Ag-Gilang@users.noreply.github.com>
2021-11-20 21:46:14 +0700

    Delete anu.js

--- plugins/anu.js
+++
@@ -1,6 +0,0 @@
-// anu
-    let reg = /(p|hi|hai|halo|hola|bot)/i
-    let isAnu = reg.exec(m.text)
-    if (isAnu && !m.fromMe) {
-        m.reply(`Ya, ada yang bisa saya bantu? kalo bingung ketik #menu ya kak`)
-    }
